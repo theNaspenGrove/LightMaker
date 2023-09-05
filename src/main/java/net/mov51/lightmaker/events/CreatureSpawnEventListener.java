a0f6e76a478d95a24d7df30789fc88e6eb974518
mov <mov51@mov51.net>
2023-09-05 02:24:29 -0400

    Fix mob spawn blocking

--- src/main/java/net/mov51/lightmaker/events/CreatureSpawnEventListener.java
+++ src/main/java/net/mov51/lightmaker/events/CreatureSpawnEventListener.java
@@ -14,24 +14,28 @@ import java.util.List;
 public class CreatureSpawnEventListener implements Listener {
     @EventHandler
     public void onCreatureSpawnEvent(CreatureSpawnEvent e) {
-        if(e.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL ||
+        if(
+                e.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL ||
                 e.getSpawnReason() != CreatureSpawnEvent.SpawnReason.REINFORCEMENTS ||
                 e.getSpawnReason() != CreatureSpawnEvent.SpawnReason.RAID ||
-                e.getSpawnReason() !=CreatureSpawnEvent.SpawnReason.VILLAGE_INVASION){return;}
-        //Check if mob intersects with any lights
-        List<Location> toCheck = new ArrayList<>();
-        for (double x = Math.floor(e.getEntity().getBoundingBox().getMinX()); x <= Math.ceil(e.getEntity().getBoundingBox().getMaxX()); x++) {
-            for (double y = Math.floor(e.getEntity().getBoundingBox().getMinY()); y <= Math.ceil(e.getEntity().getBoundingBox().getMaxY()); y++) {
-                for (double z = Math.floor(e.getEntity().getBoundingBox().getMinZ()); z <= Math.ceil(e.getEntity().getBoundingBox().getMaxZ()); z++) {
-                    toCheck.add(new Location(e.getEntity().getWorld(), x, y, z));
+                e.getSpawnReason() != CreatureSpawnEvent.SpawnReason.VILLAGE_INVASION
+        ) {
+            //Check if mob intersects with any lights
+            List<Location> toCheck = new ArrayList<>();
+            for (double x = Math.floor(e.getEntity().getBoundingBox().getMinX()); x <= Math.ceil(e.getEntity().getBoundingBox().getMaxX()); x++) {
+                for (double y = Math.floor(e.getEntity().getBoundingBox().getMinY()); y <= Math.ceil(e.getEntity().getBoundingBox().getMaxY()); y++) {
+                    for (double z = Math.floor(e.getEntity().getBoundingBox().getMinZ()); z <= Math.ceil(e.getEntity().getBoundingBox().getMaxZ()); z++) {
+                        toCheck.add(new Location(e.getEntity().getWorld(), x, y, z));
+                    }
                 }
             }
-        }
-        for(Location l : toCheck){
-            Block block = l.getBlock();
-            if(block.getType() == org.bukkit.Material.LIGHT && ((Light)block.getBlockData()).getLevel() == 0){
-                e.setCancelled(true);
-                return;
+            System.out.println(toCheck);
+            for (Location l : toCheck) {
+                Block block = l.getBlock();
+                if (block.getType() == org.bukkit.Material.LIGHT && ((Light) block.getBlockData()).getLevel() == 0) {
+                    e.setCancelled(true);
+                    return;
+                }
             }
         }
     }
